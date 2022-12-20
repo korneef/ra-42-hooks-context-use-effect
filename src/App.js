d64e75ba1e4a339eddaacce24d5cfc86185884e7
Valentin Korneev <v.korneef@gmail.com>
2022-12-20 21:17:50 +0600

    initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AppReact from './components/App';
+
+function App() {
+  return (
+    <div className="App">
+      <AppReact/>
+    </div>
+  );
+}
+
+export default App;
