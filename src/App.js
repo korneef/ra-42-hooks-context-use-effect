908b9c2a2ac504fe3c23d7f61ac20b186f53a557
Valentin Korneev <v.korneef@gmail.com>
2022-12-20 23:12:00 +0600

    homework complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
-import AppReact from './components/App';
+import { Details, List } from './components/index.js';
+import { useEffect, useState } from 'react';
+
 
 function App() {
+  const [users, setUsers] = useState({
+    allUsers: [],
+    activeUser: null
+  })
+
+  useEffect(() => {
+    fetch('https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/users.json')
+      .then((resolve) => resolve.json())
+      .then(loadUsers => {
+        setUsers({...users, allUsers: loadUsers})
+      });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  const handleChangeUser = (id) => {
+    setUsers({...users, activeUser: id})
+  }
+
   return (
     <div className="App">
-      <AppReact/>
+      <List users={users.allUsers} activeUser={users.activeUser} handleChangeUser={handleChangeUser}/>
+      {users.activeUser === null ? null : <Details info={{id: users.activeUser}}/>}
     </div>
   );
 }
